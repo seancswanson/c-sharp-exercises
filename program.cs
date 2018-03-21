9d97e0bad6162d83814c6bf1da41cd43aecd7721
Sean Swanson <sean-swan@hotmail.com>
2018-03-21 12:55:16 -0700

    First commit'

---
+++ program.cs
@@ -0,0 +1,20 @@
+ using System;
+
+ namespace FirstConsoleProject
+ {
+     class MainClass
+     {
+         public static void Main (string[] args)
+         {
+             Console.WriteLine ("Hey there! My name is Sean.");
+             Console.WriteLine ("I am 23 years old.");
+             Console.WriteLine ("----------------------------")
+             Console.WriteLine ("Please Type 'next' to go on.")
+             Console.WriteLine ("----------------------------")
+             Console.ReadLine ("next");
+             Console.WriteLine ("Great job!");
+             Console.WriteLine ("Did I mention that my favorite color is purple?");
+             Console.ReadKey ();
+         }
+     }
+ }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# C# Practice Programs
+
+## Hooray for learning!
+
+## Will be exploring Unity and C# together to make all sorts of games, educational, arcade, etc,.
