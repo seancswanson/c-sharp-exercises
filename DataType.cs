9fb9f9c668a1a02de5bcdd4d80e5b6afd69a4573
Sean Swanson <sean-swan@hotmail.com>
2018-03-21 13:08:13 -0700

    Edited Hello World for improper ReadLine syntax, delcared some variables in DataType

---
+++ DataType.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespae SecondConsoleProject
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            int num01;
+            double decimalpoint = 10.4;
+            float afloat = 10.2;
+            bool true = false;
+            string hello = "hello"
+            char h = "h"
+        }
+    }
+}
--- Hello World.cs
+++ Hello World.cs
@@ -8,11 +8,11 @@
          {
              Console.WriteLine ("Hey there! My name is Sean.");
              Console.WriteLine ("I am 23 years old.");
-             Console.WriteLine ("----------------------------")
-             Console.WriteLine ("Please Type 'next' to go on.")
-             Console.WriteLine ("----------------------------")
-             Console.ReadLine ("next");
-             Console.WriteLine ("Great job!");
+             Console.WriteLine (" --------------------------- ")
+             Console.WriteLine ("<Please press space to go on.>")
+             Console.WriteLine (" --------------------------- ")
+             Console.ReadKey ("next");
+             Console.WriteLine ("\nGreat job!");
              Console.WriteLine ("Did I mention that my favorite color is purple?");
              Console.ReadKey ();
          }
